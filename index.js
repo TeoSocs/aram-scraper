6225b6c7a531ad1f984ca1dac3b3a079564bf274
Matteo Sovilla <matteo.sovilla@infocamere.it>
2018-11-14 17:12:57 +0100

    finished webserver

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(tierList));
+  res.end();
+}).listen(8080);
+
 const BASE_URL = "https://www.metasrc.com";
 const URL = BASE_URL + "/aram/tierlist";
 
 let tierList = {}
 
-rp(URL).then(function (html) {
-  raw_champ = $('.champion-grid-item', html);
-  for (let i = 0; i < raw_champ.length; i++) {
-    let champ = {};
-    
-    champ.name = $('div > div', raw_champ[i].attribs["title"]).text();
-    champ.link = BASE_URL + raw_champ[i].children[0].attribs["href"];
-    champ.imgUrl = raw_champ[i].children[0].children[0].attribs["src"];
-    champ.description = raw_champ[i].children[0].children[0].attribs["alt"];
-    champ.tier = $('.tier', raw_champ[i].attribs["title"]).text();
+function fetch() {
+  rp(URL).then(function (html) {
+    tierList = {}
+    raw_champ = $('.champion-grid-item', html);
+    for (let i = 0; i < raw_champ.length; i++) {
+      let champ = {};
 
-    if (tierList[champ.tier] == null) {
-      tierList[champ.tier] = [];
+      champ.name = $('div > div', raw_champ[i].attribs["title"]).text();
+      champ.link = BASE_URL + raw_champ[i].children[0].attribs["href"];
+      champ.imgUrl = raw_champ[i].children[0].children[0].attribs["src"];
+      champ.description = raw_champ[i].children[0].children[0].attribs["alt"];
+      champ.tier = $('.tier', raw_champ[i].attribs["title"]).text();
+
+      if (tierList[champ.tier] == null) {
+        tierList[champ.tier] = [];
+      }
+      tierList[champ.tier].push(champ);
     }
-    tierList[champ.tier].push(champ);
-  }
-  console.log(tierList);
-}).catch(function (err) {
-  console.log(err)
-});
+    console.log(tierList);
+  }).catch(function (err) {
+    console.log(err)
+  });
+}
+
+fetch()
+
+setInterval(fetch, 30000)
