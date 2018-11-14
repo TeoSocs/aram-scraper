872e4f7d08411a56fc0d36e082f59cc00c19f287
TeoSocs <teo.sovi@gmail.com>
2018-11-14 11:42:30 +0100

    completato scraping

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ class ChampionData {
 
 rp(URL).then(function (html) {
   raw_champ = $('.champion-grid-item', html);
-  console.log(raw_champ[0].children[0].children);
+  console.log();
   for (let i = 0; i < raw_champ.length; i++) {
     let champ = new ChampionData();
-    // champ.name = raw_champ[0].children;
+    
+    champ.name = $('div > div', raw_champ[i].attribs["title"]).text();
     champ.link = raw_champ[i].children[0].attribs["href"];
+    champ.imgUrl = raw_champ[i].children[0].children[0].attribs["src"];
+    champ.description = raw_champ[i].children[0].children[0].attribs["alt"];
 
     tier = $('.tier', raw_champ[i].attribs["title"]).text();
+
     if (tierList[tier] == null) {
       tierList[tier] = [];
     }
-    tierList[tier].push(i);
+    tierList[tier].push(champ);
   }
   console.log(tierList);
 }).catch(function (err) {
